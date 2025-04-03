438d69f05fae1cc661d1fe0a535669295516181c
cyrraz <code.cyrraz@protonmail.com>
2025-04-03 16:38:01 +0900

    docs: redirect to the test folder

--- src/test_fourier_transform/__init__.py
+++ src/test_fourier_transform/__init__.py
@@ -1,6 +1,4 @@
 import logging
 
-
-def main() -> None:
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Hello from test-fourier-transform!")
+logging.basicConfig(level=logging.INFO)
+logging.info("Have a look into the test folder.")
